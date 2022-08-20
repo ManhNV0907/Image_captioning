b78e961926422f9e7c4838e316efae2bc997d080
BambooKidFr <manhqh0909@gmail.com>
2022-08-20 15:31:15 +0700

    Update  get data scripts

---
+++ getData.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import os
+
+#Download caption annotation files
+annotation_folder = '/annotations/'
+if not os.path.exists(os.path.abspath('.') + annotation_folder):
+  annotation_zip = tf.keras.utils.get_file('captions.zip',
+                                           cache_subdir=os.path.abspath('.'),
+                                           origin='http://images.cocodataset.org/annotations/annotations_trainval2014.zip',
+                                           extract=True)
+  annotation_file = os.path.dirname(annotation_zip)+'/annotations/captions_train2014.json'
+  
+  os.remove(annotation_zip)
+#Validset
+image_folder = '/val2014/'
+if not os.path.exists(os.path.abspath('.') + image_folder):
+  image_zip = tf.keras.utils.get_file('val2014.zip',
+                                      cache_subdir=os.path.abspath('.'),
+                                      origin='http://images.cocodataset.org/zips/val2014.zip',
+                                      extract=True)
+  PATH = os.path.dirname(image_zip) + image_folder
+  os.remove(image_zip)
+else:
+  PATH = os.path.abspath('.') + image_folder
+#Trainset
+image_folder = '/train2014/'
+if not os.path.exists(os.path.abspath('.') + image_folder):
+  image_zip = tf.keras.utils.get_file('train2014.zip',
+                                      cache_subdir=os.path.abspath('.'),
+                                      origin='http://images.cocodataset.org/zips/train2014.zip',
+                                      extract=True)
+  PATH = os.path.dirname(image_zip) + image_folder
+  os.remove(image_zip)
+else:
+  PATH = os.path.abspath('.') + image_folder
---
+++ getData.sh
@@ -0,0 +1,5 @@
+import os
+os.makedirs('./coco', exist_ok=True)
+cd coco
+#!/bin/sh
+python getData.py
